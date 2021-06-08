1d126380e5bd419743670466872b4b40bea02c5c
VietHoang <hoangcf1134@gmail.com>
2021-06-08 21:52:38 +0700

    Commit All

--- LuxStay/Areas/Admin/AdminAreaRegistration.cs
+++ LuxStay/Areas/Admin/AdminAreaRegistration.cs
@@ -17,7 +17,7 @@ namespace LuxStay.Areas.Admin
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
+                new { action = "Index",Controller="Dashboard" ,id = UrlParameter.Optional }
             );
         }
     }
