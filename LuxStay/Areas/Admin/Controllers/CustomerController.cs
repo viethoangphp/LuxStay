e5bc9932b4af7792a5219fbe252ae13e1a36102b
Nguyễn Thanh Tú <ntt11052000@gmail.com>
2021-06-10 18:23:44 +0700

    Import UI

---
+++ LuxStay/Areas/Admin/Controllers/CustomerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LuxStay.Areas.Admin.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Admin/Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
