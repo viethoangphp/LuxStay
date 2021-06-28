49440935c262ac70e97ad849b461782bc9a1d40e
TuNguyen16 <ntt11052000@gmail.com>
2021-06-28 21:01:15 +0700

    Post 50% complete (no controller code and editor yet)

---
+++ LuxStay/Areas/Admin/Controllers/PostController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LuxStay.Areas.Admin.Controllers
+{
+    public class PostController : BaseController
+    {
+        // GET: Admin/Post
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
